78601558ea8baaed84afcd1aed6c52263aa8ded2
kennedy-vieira <48939384+kennedy-vieira@users.noreply.github.com>
2019-06-09 18:09:34 -0300

    Update and rename Gwent.java to gwentGame/target/classes/orientacaoobjetovitorkennedy/gwent/Gwent.java

--- gwentGame/target/classes/orientacaoobjetovitorkennedy/gwent/Gwent.java
+++ gwentGame/target/classes/orientacaoobjetovitorkennedy/gwent/Gwent.java
@@ -16,7 +16,7 @@ public class Gwent {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hello world");
+        
     }
     
 }
