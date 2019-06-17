24a944372079b92a150374067fa17ff8c3532e5b
kennedy-vieira <48939384+kennedy-vieira@users.noreply.github.com>
2019-06-17 15:42:51 -0300

    Update Mao.java
    Edição no nome do pacote

--- gwentGame/src/main/java/gwent/Mao.java
+++ gwentGame/src/main/java/gwent/Mao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package orientacaoobjetovitorkennedy.gwentgame;
+package gwent;
 import java.util.ArrayList;
 
 
